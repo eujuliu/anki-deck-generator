f621ce2ce217318302b38f6750db68eb06029aee
Julio Martins <juliomarttins5@gmail.com>
2025-03-12 19:08:27 -0300

    feat: added setup file for install the cli

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+def read_requirements():
+    with open("requirements.txt") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
+setup(
+    name="anki-deck-generator",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    entry_points={
+        "console_scripts": [
+            "anki_deck=anki_deck_generator.__main__:main",
+        ],
+    },
+)
